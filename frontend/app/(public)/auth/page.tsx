772acc735d940718f5d8d54703ffa35392827c12
Vinicius Madeira <viniciussmadeira@gmail.com>
2025-04-05 13:58:56 -0300

    add: temporary Auth page

--- frontend/app/(public)/auth/page.tsx
+++ frontend/app/(public)/auth/page.tsx
@@ -1,3 +1,38 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function Auth() {
-  return <div>Auth Page</div>;
+  const [counter, setCounter] = useState(3);
+  const router = useRouter();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prev) => {
+        if (prev <= 1) {
+          clearInterval(interval);
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval); // Cleanup if component unmounts early
+  }, []);
+
+  useEffect(() => {
+    if (counter === 0) router.push("/dashboard");
+  }, [counter]);
+
+  return (
+    <div className="grid grid-rows-[1fr_20px] justify-items-center p-4 mt-50">
+      <h1 className="font-bold text-5xl mb-4">
+        Esta página estará disponível na versão 2.0!
+      </h1>
+      <h4 className="text-2xl text-neutral-400">
+        Você será redirecionado em {counter} segundos.
+      </h4>
+    </div>
+  );
 }
